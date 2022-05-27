1838319f0e3fb52e9110f41ab7bd997c1aab6c0f
Vladislav Polekha <vladsimplemente@gmail.com>
2022-05-27 21:16:02 +0300

    feat(Redux): add redux

---
+++ src/api/makeRequest.ts
@@ -0,0 +1,5 @@
+const get = (url: string) => ({ userId: '7450d485-c4c5-4d1e-8d65-588aa95c94b8', age: 22 });
+
+const post = (url: string) => ({ status: 'completed' });
+
+export default { get, post };
---
+++ src/redux/actionCreator.ts
@@ -0,0 +1,3 @@
+export const activateUser = () => ({ type: 'activate', payload: { name: 'test' } });
+
+export const deactivateUser = () => ({ type: 'deactivate' });
